758690b5a0a874f70ca07487a091d34ec210f5df
Parag Chirde <parag.chirde@gmail.com>
2020-05-10 13:00:00 +0530

    Instant.page Added

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import 'vuesax/dist/vuesax.css'
 import PatterCSS from 'pattern.css/dist/pattern.css'
 
 export default function(Vue, { router, head, isClient, appOptions }) {
+    // Add an external Javascript before the closing </body> tag
+    head.script.push({
+        src: '//instant.page/3.0.0',
+        type: 'module',
+        integrity: 'sha384-OeDn4XE77tdHo8pGtE1apMPmAipjoxUQ++eeJa6EtJCfHlvijigWiJpD7VDPWXV1',
+        body: true
+    })
 
     Vue.use(Vuesax)
     Vue.use(PatterCSS)
